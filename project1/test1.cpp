0e06000b3d6c7c100ed1fdbc9f06e5fb7860dbea
chxw <chexuhuwa@gmail.com>
2019-05-28 14:09:06 -0400

    REV: testing functions added to test1; CardDeck toString() functional

--- project1/test1.cpp
+++ project1/test1.cpp
@@ -1,39 +1,68 @@
 #include <iostream>
 #include <string>
 
+#include <stdlib.h>     /* srand, rand */
+
 #include "Card.cpp"
 #include "CardDeck.cpp"
 
-CardDeck create_cards(){
-	Card c1("SPADE", 1);
-	Card c2("SPADE", 2);
-	Card c3("SPADE", 3);
-	Card c4("SPADE", 4);
-	Card c5("SPADE", 5);
-	Card c6("SPADE", 6);
-	Card c7("SPADE", 7);
-	Card c8("SPADE", 8);
-	Card c9("SPADE", 9);
-	Card c10("SPADE", 10);
-	Card c11("SPADE", 11);
-	Card c12("SPADE", 12);
-	Card c13("SPADE", 13);
+CardDeck create_suit(std::string s){
+	std::string suit = s;
 
-	Card spades[13] = {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13};
+	Card c1(suit, 1);
+	Card c2(suit, 2);
+	Card c3(suit, 3);
+	Card c4(suit, 4);
+	Card c5(suit, 5);
+	Card c6(suit, 6);
+	Card c7(suit, 7);
+	Card c8(suit, 8);
+	Card c9(suit, 9);
+	Card c10(suit, 10);
+	Card c11(suit, 11);
+	Card c12(suit, 12);
+	Card c13(suit, 13);
 
-	CardDeck spades_deck;
+	Card cards[13] = {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13};
+
+	CardDeck deck;
 
 	for(int i = 0; i < 13; i++){
-		spades_deck.add(spades[i]);
+		deck.add(cards[i]);
 	}
 
-	return spades_deck;
+	return deck;
 }
 
 int main(){
-	CardDeck spades = create_cards();
-
+	CardDeck spades = create_suit("SPADE");
 	std::cout << spades.toString() << std::endl;
+
+	CardDeck hearts = create_suit("HEARTS");
+	std::cout << hearts.toString() << std::endl;
+
+	CardDeck clubs = create_suit("CLUBS");
+	std::cout << clubs.toString() << std::endl;
+
+	CardDeck diamonds = create_suit("DIAMONDS");
+	std::cout << diamonds.toString() << std::endl;
+
+	CardDeck deck1;
+
+	int count = 0;
+	while(count < 12){
+		int a = rand() % 13 + 1;
+		deck1.add(spades.at(a));
+		int b = rand() % 13 + 1;
+		deck1.add(hearts.at(b));
+		int c = rand() % 13 + 1;
+		deck1.add(clubs.at(c));
+		int d = rand() % 13 + 1;
+		deck1.add(diamonds.at(d));
+		count += 4;
+	}
+
+	std::cout << deck1.toString() << std::endl;
 
 	return 0;
 }
