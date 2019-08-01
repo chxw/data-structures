4a3e4718bd480428c988ede98b7049404a9411ea
chxw <chexuhuwa@gmail.com>
2019-08-01 11:42:40 -0400

    REV: updated plan, testing make build

--- project5/main.cpp
+++ project5/main.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 
+#include<vector>
+
 int main(int argc, char* argv[]){
   if(argc != 2){
     std::cout << "Error" << std::endl;
@@ -12,7 +14,19 @@ int main(int argc, char* argv[]){
   		std::cout << "Failed to open file" << std::endl;
   	}
   	else{
-  		file.open(argv[1], std::ios::in);
+  		// file.open(argv[1], std::ios::in);
+  		std::string line;
+
+  		while(std::getline(file, line)){
+  			std::vector<string> row;
+  			split(line, '\t', row);
+
+  			std::string word = row.at(0);
+  			int freq = row.at(1);
+
+  			std::cout << "word is " << word << std::endl;
+  			std::cout << "freq is " << freq << std::endl;
+  		}
   	}
     std::cout << argv[1] << std::endl;
   }
