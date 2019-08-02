5e700b05d4bc0333876c208ed7097e8c08b38fc3
chxw <chexuhuwa@gmail.com>
2019-08-02 09:30:20 -0400

    REV: commented out lines in l10 test

--- lab10/test.cpp
+++ lab10/test.cpp
@@ -12,11 +12,11 @@ void test1(){
   graph.addEdge(1, 2, 1);
   graph.addEdge(2, 0, 1);
 
-  graph.setShortestPathsFrom(0);
+//  graph.setShortestPathsFrom(0);
 
-  assert(graph.getShortestPath(0, 0) == "0 (0)");
-  assert(graph.getShortestPath(0, 1) == "0->1 (1)");
-  assert(graph.getShortestPath(0, 2) == "0->1->2 (2)");
+//  assert(graph.getShortestPath(0, 0) == "0 (0)");
+//  assert(graph.getShortestPath(0, 1) == "0->1 (1)");
+//  assert(graph.getShortestPath(0, 2) == "0->1->2 (2)");
 }
 
 void test2(){
@@ -29,11 +29,11 @@ void test2(){
   graph.addEdge(0, 2, 1);
   graph.addEdge(1, 2, 1);
 
-  graph.setShortestPathsFrom(0);
+//  graph.setShortestPathsFrom(0);
 
-  assert(graph.getShortestPath(0, 0) == "0 (0)");
-  assert(graph.getShortestPath(0, 1) == "0->1 (1)");
-  assert(graph.getShortestPath(0, 2) == "0->2 (1)");
+//  assert(graph.getShortestPath(0, 0) == "0 (0)");
+//  assert(graph.getShortestPath(0, 1) == "0->1 (1)");
+//  assert(graph.getShortestPath(0, 2) == "0->2 (1)");
 }
 
 void test3(){
@@ -59,16 +59,16 @@ void test3(){
   graph.addEdge(5, 7, 2);
   graph.addEdge(6, 7, 3);
 
-  graph.setShortestPathsFrom(0);
+//  graph.setShortestPathsFrom(0);
 
-  assert(graph.getShortestPath(0, 0) == "0 (0)");
-  assert(graph.getShortestPath(0, 1) == "0->1 (1)");
-  assert(graph.getShortestPath(0, 2) == "0->2 (4)");
-  assert(graph.getShortestPath(0, 3) == "0->1->3 (3)");
-  assert(graph.getShortestPath(0, 4) == "0->2->4 (7)");
-  assert(graph.getShortestPath(0, 5) == "0->2->5 (8)");
-  assert(graph.getShortestPath(0, 6) == "0->2->4->6 (9)");
-  assert(graph.getShortestPath(0, 7) == "0->2->5->7 (10)");
+//  assert(graph.getShortestPath(0, 0) == "0 (0)");
+//  assert(graph.getShortestPath(0, 1) == "0->1 (1)");
+//  assert(graph.getShortestPath(0, 2) == "0->2 (4)");
+//  assert(graph.getShortestPath(0, 3) == "0->1->3 (3)");
+//  assert(graph.getShortestPath(0, 4) == "0->2->4 (7)");
+//  assert(graph.getShortestPath(0, 5) == "0->2->5 (8)");
+//  assert(graph.getShortestPath(0, 6) == "0->2->4->6 (9)");
+//  assert(graph.getShortestPath(0, 7) == "0->2->5->7 (10)");
 }
 
 int main(){
